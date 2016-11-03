7021966faee70dd6b4262a486bf1175cbf028c07
Martin Hvidberg <mhn@miracle.dk>
2016-11-03 11:08:03 +0100

    implemented log4net

--- Core.ApplicationServices/Logger/Logger.cs
+++ Core.ApplicationServices/Logger/Logger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,44 +7,49 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.ApplicationServices.Logger
-{
-    public class Logger : ILogger
     {
-        public void Log(string msg, string fileName)
+        public class Logger : ILogger
         {
-            using (var file = new StreamWriter("c://logs//os2eindberetning//" + fileName + ".log", true))
+            private ILog log;
+
+            public Logger()
             {
-                var time = DateTime.Now.ToString();
-                file.WriteLine(time + " : " + msg);
-                file.Close();
+                log = LogManager.GetLogger("Logger");
+            }
+
+            public void Log(string msg, string fileName)
+            {
+                log.Info(msg);
+            }
+
+            public void Log(string msg, string fileName, Exception ex)
+            {
+                log.Error(msg, ex);
+            }
+
+            public void Log(string msg, string fileName, Exception ex, int level)
+            {
+                var message = "[Niveau " + level + "] - " + msg;
+                switch (level)
+                {
+                    case 1: log.Error(message, ex); break;
+                    case 2: log.Warn(message, ex); break;
+                    default:
+                        log.Info(message, ex); break;
+                }
+            }
+
+            public void Log(string msg, string fileName, int level)
+            {
+                var message = "[Niveau " + level + "] - " + msg;
+                switch (level)
+                {
+                    case 1: log.Error(message); break;
+                    case 2: log.Warn(message); break;
+                    default:
+                        log.Info(message); break;
+                }
             }
         }
 
-        public void Log(string msg, string fileName, Exception ex)
-        {
-            using (var file = new StreamWriter("c://logs//os2eindberetning//" + fileName + ".log", true))
-            {
-                var time = DateTime.Now.ToString();
-                file.WriteLine(time + " : " + msg);
-                if (ex != null) file.WriteLine(ex);
-                file.Close();
-            }
-        }
-
-        public void Log(string msg, string fileName, Exception ex, int level)
-        {
-            using (var file = new StreamWriter("c://logs//os2eindberetning//" + fileName + ".log", true))
-            {
-                var time = DateTime.Now.ToString();
-                file.WriteLine(time + " : " + "[Niveau " + level + "] - " + msg);
-                if (ex != null) file.WriteLine(ex);
-                file.Close();
-            }
-        }
-
-        public void Log(string msg, string fileName, int level)
-        {
-            Log(msg, fileName, null, level);
-        }
     }
-}
